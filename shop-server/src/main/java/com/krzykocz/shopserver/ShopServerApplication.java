1747d1d1138c074902af3b4c78e882a2641ba082
Krzysztof Koczwara <krzykocz@gmail.com>
2019-03-19 23:30:36 +0100

    Start to rebuil api

---
+++ shop-server/src/main/java/com/krzykocz/shopserver/ShopServerApplication.java
@@ -0,0 +1,13 @@
+package com.krzykocz.shopserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopServerApplication.class, args);
+	}
+
+}
